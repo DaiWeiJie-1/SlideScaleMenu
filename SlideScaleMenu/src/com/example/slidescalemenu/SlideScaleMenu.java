ef1d8903ad269fb31b8d788a1b64107b6a8778ea
daiweijie <daiweijie@dana-tech.com>
2016-01-06 16:13:04 +0800

    test

--- SlideScaleMenu/src/com/example/slidescalemenu/SlideScaleMenu.java
+++ SlideScaleMenu/src/com/example/slidescalemenu/SlideScaleMenu.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+
 import android.widget.RelativeLayout;
 
 public class SlideScaleMenu extends FrameLayout{
